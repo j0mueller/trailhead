a1084511ca85332780fd7f198e57decb0d9cc1d3
Joanne Mueller <joannemueller@mac.com>
2017-07-30 18:44:20 -0400

    add placeholder hike page and install foundation

--- app/controllers/api/v1/hikes_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::HikesController < ApplicationController
-  # skip_before_action :verify_authenticity_token
-
-  def index
-    render json: Hike.all, adapter: :json
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#index'
+  root 'hikes#index'
 
-  namespace :api do
-    namespace :v1 do
-      resources :hikes, only: [:index]
-    end
-  end
+  resources :hikes, only: [:index, :show]
 
 end

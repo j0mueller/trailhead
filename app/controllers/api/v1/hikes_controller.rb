fd1c3b1e4a54ee08a3be97bd1f021eef2e53930f
Joanne Mueller <joannemueller@mac.com>
2017-07-30 13:29:15 -0400

    add hike model migration and controller

---
+++ app/controllers/api/v1/hikes_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::HikesController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+
+  def index
+    
+    render json: Hike.all, adapter: :json
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :hikes, only: [:index]
+    end
+  end
+
 end

b0dfeb6b2d1e93ac5f7485fecca05ac73c823b20
Joanne Mueller <joannemueller@mac.com>
2017-08-03 11:09:29 -0400

    add wishlist

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   # get '/hikes', to: 'static_pages#index'
 
   resources :hikes, only: [:index, :show]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :wishlists, only: [:show]
+  end
 
   namespace :api do
     namespace :v1 do
